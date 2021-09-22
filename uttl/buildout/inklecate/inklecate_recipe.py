fcdeb0d2b6c605387baf403319b3bc328cce100e
Mr. Hands <mrhands31@gmail.com>
2021-09-22 15:38:04 +0200

    Clean up inklecate recipe and add documentation.

--- uttl/buildout/inklecate/inklecate_recipe.py
+++ uttl/buildout/inklecate/inklecate_recipe.py
@@ -1,61 +1,56 @@
 from zc.buildout import UserError
-from subprocess import CalledProcessError
-
-import logging
 import glob
 import os
 import re
 import subprocess
 
-class InklecateRecipe(object):
+from uttl.buildout.install_recipe import InstallRecipe
+from zc.buildout import UserError
+from subprocess import CalledProcessError
+
+class InklecateRecipe(InstallRecipe):
 	def __init__(self, buildout, name, options):
-		self.buildout, self.name, self.options = buildout, name, options
-		self.log = logging.getLogger(self.name)
+		super().__init__(buildout, name, options)
 
 		self.options.setdefault('executable', 'inklecate.exe')
 		self.options.setdefault('output_directory', '')
 
-		self.inputs = self.options['inputs'].splitlines()
+		# resolve input files
 
-		self.resolved = []
-		for i in self.inputs:
-			self.resolved.extend([os.path.abspath(f) for f in glob.glob(i) if os.path.isfile(f)])
+		if not 'input' in self.options:
+			raise UserError('Missing mandatory "input" parameter.')
 
-		self.options['inputs_resolved'] = ' '.join(str(e) for e in self.resolved)
+		self.input_resolved = []
+		for i in self.options['input'].splitlines():
+			self.input_resolved.extend([os.path.abspath(f) for f in glob.glob(i) if os.path.isfile(f)])
+
+		self.options['input_resolved'] = ' '.join(str(e) for e in self.input_resolved)
 
 	def install(self):
-		build_inputs = []
-
-		# check if any artefact is missing
+		# resolve output files
 
 		output_directory = self.options['output_directory']
 
-		for i in self.resolved:
+		for i in self.input_resolved:
 			if not os.path.exists(i):
 				continue
 
-			filename = os.path.split(i)[1]
+			input_path = os.path.abspath(i)
+			filename = os.path.split(input_path)[1]
 			artefact_path = os.path.join(output_directory, os.path.splitext(filename)[0] + '.json')
 
 			self.options.created(artefact_path)
 
-			if not os.path.exists(artefact_path):
-				build_inputs.append((os.path.abspath(i), artefact_path))
-				continue
+			# compile ink to json
 
-		# compile
-
-		for ink_in, json_out in build_inputs:
 			args = [ self.options['executable'] ]
-			args.extend([ '-o', json_out ])
-			args.extend([ ink_in ])
+			args.extend([ '-o', artefact_path ])
+			args.extend([ input_path ])
 
 			if not self.runCommand(args):
 				raise CalledProcessError(0, args)
 
 		return self.options.created()
-
-	update = install
 
 	def runCommand(self, args):
 		self.log.debug(str(args))
