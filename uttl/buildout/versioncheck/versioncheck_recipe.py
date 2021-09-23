a96654e995ca30baf96f8d6eefd2e209a0c40123
Mr. Hands <mrhands31@gmail.com>
2021-09-23 11:34:34 +0200

    Clean up versioncheck and add documentation.

--- uttl/buildout/versioncheck/versioncheck_recipe.py
+++ uttl/buildout/versioncheck/versioncheck_recipe.py
@@ -1,18 +1,15 @@
 import configparser
-import logging
 import os.path
 import re
 import subprocess
 import types
-import zc.buildout
-import winreg
 
+from uttl.buildout.base_recipe import BaseRecipe
 from zc.buildout import UserError
 
-class VersionCheckRecipe(object):
+class VersionCheckRecipe(BaseRecipe):
 	def __init__(self, buildout, name, options):
-		self.buildout, self.name, self.options = buildout, name, options
-		self.log = logging.getLogger(self.name)
+		super().__init__(buildout, name, options)
 
 		self.options.setdefault('version_file', name + '.ini')
 		self.options.setdefault('required_major', '0')
@@ -39,47 +36,58 @@ class VersionCheckRecipe(object):
 		f = types.MethodType(eval('checkVersion'), self)
 		setattr(self, 'checkVersion', f)
 
-		# create version file
+		# version file
 
-		self.version_path = os.path.join(buildout['buildout']['parts-directory'], self.options['version_file'])
+		self.version_file = os.path.join(buildout['buildout']['parts-directory'], self.options['version_file'])
+
+		# check version using script method
+
+		success, self.version_major, self.version_minor, self.version_debug, self.path = self.checkVersion()
+		if not success:
+			raise UserError('Failed to check version.')
+
+		self.options['version_major'] = str(self.version_major)
+		self.options['version_minor'] = str(self.version_minor)
+		self.options['version_debug'] = str(self.version_debug)
+		self.options['path'] = self.path
+
+		self.log.debug('path %s version %s.%s.%s' % (self.options['path'], self.options['version_major'], self.options['version_minor'], self.options['version_debug']))
+
+	def install(self):
+		self.options.created(self.version_file)
+
+		# read or create version object
 
 		self.version = configparser.ConfigParser()
-
-		if os.path.exists(self.version_path):
-			self.version.read_file(open(self.version_path))
-			self.options['path'] = self.version['location']['path']
-			self.options['version_major'] = self.version['version']['major']
-			self.options['version_minor'] = self.version['version']['minor']
-			self.options['version_debug'] = self.version['version']['debug']
+		if os.path.exists(self.version_file):
+			self.version.read_file(open(self.version_file))
 		else:
-			success, major, minor, debug, self.options['path'] = self.checkVersion()
-			if not success:
-				raise zc.buildout.UserError('Failed to check version')
+			self.version['version'] = {
+				'major': '0',
+				'minor': '0',
+				'debug': '0'
+			}
+
+		# check for newer version
+
+		if int(self.options['version_major']) > int(self.version['version']['major']) or int(self.options['version_minor']) > int(self.version['version']['minor']) or int(self.options['version_debug']) > int(self.version['version']['debug']):
+			version_old = '%s.%s.%s' % (self.version['version']['major'], self.version['version']['minor'], self.version['version']['debug'])
+			version_new = '%s.%s.%s' % (self.options['version_major'], self.options['version_minor'], self.options['version_debug'])
+			self.log.info('Found newer version: %s > %s' % (version_old, version_new))
 
 			self.version['version'] = {
-				'major': str(major),
-				'minor': str(minor),
-				'debug': str(debug)
+				'major': self.options['version_major'],
+				'minor': self.options['version_minor'],
+				'debug': self.options['version_debug']
 			}
 			self.version['location'] = {
 				'path': self.options['path']
 			}
 
-			self.options['version_major'] = self.version['version']['major']
-			self.options['version_minor'] = self.version['version']['minor']
-			self.options['version_debug'] = self.version['version']['debug']
-
-			with open(self.version_path, 'w+') as f:
+			with open(self.version_file, 'w+') as f:
 				self.version.write(f)
 
-		self.log.debug('path %s version %s.%s.%s' % (self.options['path'], self.options['version_major'], self.options['version_minor'], self.options['version_debug']))
-
-	def install(self):
-		self.options.created(self.version_path)
-
 		# check version against required
-
-		self.version.read(self.version_path)
 
 		if int(self.version['version']['major']) < int(self.options['required_major']) or int(self.version['version']['minor']) < int(self.options['required_minor']):
 			self.log.error('Dependency at %s.%s is out of date, >= %d.%d is required' % (
@@ -87,7 +95,7 @@ class VersionCheckRecipe(object):
 				int(self.version['version']['minor']),
 				int(self.options['required_major']),
 				int(self.options['required_minor'])))
-			raise zc.buildout.UserError('Version mismatch')
+			raise UserError('Version mismatch')
 
 		return self.options.created()
 
