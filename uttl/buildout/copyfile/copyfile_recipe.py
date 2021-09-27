d96b81a389be57431ef68c8d1427b884e828bc2c
Mr. Hands <mrhands31@gmail.com>
2021-09-27 10:50:45 +0200

    Update copyfile recipe.

--- uttl/buildout/copyfile/copyfile_recipe.py
+++ uttl/buildout/copyfile/copyfile_recipe.py
@@ -2,51 +2,113 @@ import logging
 import os
 import shutil
 
-class CopyFileRecipe(object):
+from uttl.buildout.base_recipe import BaseRecipe
+from zc.buildout import UserError
+
+class CopyFileRecipe(BaseRecipe):
 	def __init__(self, buildout, name, options):
-		self.buildout, self.name, self.options = buildout, name, options
-		self.log = logging.getLogger(self.name)
+		super().__init__(buildout, name, options)
 
-		self.options.setdefault('destination', os.getcwd())
 		self.options.setdefault('source-path', '')
+		self.options.setdefault('destination-path', os.getcwd())
 
-		self.files = options['files'].splitlines()
+		# paths
+
+		self.src_path = os.path.abspath(self.options['source-path'])
+
+		self.dst_path = os.path.abspath(self.options['destination-path'])
+		if not os.path.exists(self.dst_path):
+			os.makedirs(self.dst_path, 0o777, True)
+
+		# get files
+
+		if not 'files' in self.options:
+			raise UserError('Missing mandatory "files" option.')
+
+		self.files = self.options['files'].splitlines()
+		self.src_files = [os.path.join(self.src_path, file) for file in self.files]
+		self.dst_files = [os.path.join(self.dst_path, file) for file in self.files]
+
+		self.log.debug(str(self.files))
 
 	def install(self):
-		dstPath = os.path.abspath(self.options['destination'])
-		if not os.path.exists(dstPath):
-			os.makedirs(dstPath, 0o777, True)
+		installed = self.dst_files
+		invalid = []
 
-		srcPath = os.path.abspath(self.options['source-path'])
+		# use private api to check for files that need to be installed
 
-		for file in self.files:
+		(installed_part_options, installed_exists) = self.buildout._read_installed_part_options()
+
+		if self.name in installed_part_options:
+			part_options = installed_part_options[self.name]
+			if part_options and '__buildout_installed__' in part_options:
+				installed = list(path.rstrip() for path in part_options['__buildout_installed__'].split())
+
+		self.log.debug('installed ' + str(installed))
+
+		invalid = [file for file in installed if not os.path.exists(file)]
+		self.log.debug('invalid ' + str(invalid))
+
+		for dst_path in invalid:
+			filename = os.path.basename(dst_path)
+			src_path = os.path.join(self.src_path, filename)
+			self.copyFile(src_path, dst_path, filename)
+
+		# check if files were modified
+
+		check_modified = list(set(invalid) - set(installed))
+		self.log.debug('check_modified ' + str(check_modified))
+		for dst_path in check_modified:
+			filename = os.path.basename(dst_path)
+
+			src_path = os.path.join(self.src_path, filename)
+			src_modified = os.path.getmtime(src_path)
+
+			dst_modified = os.path.getmtime(dst_path)
+
+			if src_modified > dst_modified:
+				self.log.debug('%s was modified (%d > %d)' % (filename, src_modified, dst_modified))
+
+				self.copyFile(src_path, dst_path, filename)
+
+		"""
+		for file in invalid:
 			filename = os.path.basename(file)
 
 			src = os.path.abspath(file)
-			srcExists = os.path.exists(src)
-			if not srcExists:
-				src = os.path.abspath(os.path.join(srcPath, filename))
-				srcExists = os.path.exists(src)
+			src_exists = os.path.exists(src)
+			if not src_exists:
+				src = os.path.abspath(os.path.join(self.src_path, filename))
+				src_exists = os.path.exists(src)
 
-			self.log.debug('src: %s (exists: %r)' % (src, srcExists))
+			self.log.debug('src: %s (exists: %r)' % (src, src_exists))
 
-			if not srcExists:
+			if not src_exists:
 				raise FileNotFoundError(src)
 
-			dst = os.path.abspath(os.path.join(dstPath, filename))
-			dstExists = os.path.exists(dst)
+			dst = os.path.abspath(os.path.join(self.dst_path, filename))
+			dst_exists = os.path.exists(dst)
 
-			self.log.debug('dst: %s (exists: %r)' % (dst, dstExists))
+			self.log.debug('dst: %s (exists: %r)' % (dst, dst_exists))
 
 			self.options.created(dst)
 
-			if not dstExists:
-				self.log.info('Copying "' + filename + "'...")
-				shutil.copy(src, dst)
+			self.copyFile(src, dst)
 
+		"""
 		return self.options.created()
 
 	update = install
+
+	def copyFile(self, src, dst, filename):
+		if not os.path.exists(src):
+			raise FileNotFoundError(src)
+
+		self.options.created(dst)
+
+		self.log.info('Copying "' + filename + "'...")
+
+		shutil.copy(src, dst)
 
 def uninstall(name, options):
 	pass
